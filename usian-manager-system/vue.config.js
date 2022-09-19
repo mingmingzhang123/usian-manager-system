f1a53c88e7a39c342d87d72b0d68b13ec40f8673
zm <1138195675@qq.com>
2022-09-19 20:52:15 +0800

    添加development分支配置了浏览器标题配置了ico图标

--- usian-manager-system/vue.config.js
+++ usian-manager-system/vue.config.js
@@ -1,4 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
+const {
+  defineConfig
+} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    // 端口号
+    port: 8888,
+    // 主机名
+    host: "localhost",
+    // 启动项目时是否自动打开浏览器
+    open: true,
+    // 是否开启https
+    https: false,
+    // 配置跨域
+    // proxy:{
+
+    // }
+  }
 })
